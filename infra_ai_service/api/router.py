1d04f8983bc892ea436db816b2630f0ee923a3dc
loopsaaage <maskerprc@gmail.com>
2024-09-19 14:21:27 +0800

    Add status api

--- infra_ai_service/api/router.py
+++ infra_ai_service/api/router.py
@@ -6,6 +6,8 @@ from infra_ai_service.api.ai_enhance.embedding import \
     router as embedding_router
 from infra_ai_service.api.ai_enhance.vector_search import \
     router as vector_search_router
+from infra_ai_service.api.system.status import \
+    router as status_router
 
 api_router = APIRouter()
 api_router.include_router(text_process_router, prefix="/text", tags=["text"])
@@ -13,5 +15,5 @@ api_router.include_router(embedding_router, prefix="/embedding",
                           tags=["embedding"])
 api_router.include_router(vector_search_router, prefix="/search",
                           tags=["search"])
-api_router.include_router(embedding_router, prefix="/status",
+api_router.include_router(status_router, prefix="/status",
                           tags=["status"])
